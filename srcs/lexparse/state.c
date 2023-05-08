3e099338f498b9ed989e0a9922de67149ff98a7d
Nicolas BLED <nbled@e2r4p13.clusters.42paris.fr>
2023-05-08 18:38:22 +0200

    alledoskour

--- srcs/lexparse/state.c
+++ srcs/lexparse/state.c
@@ -6,17 +6,15 @@
 /*   By: nbled <nbled@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 19:33:22 by nbled             #+#    #+#             */
-/*   Updated: 2023/05/03 18:01:17 by nbled            ###   ########.fr       */
+/*   Updated: 2023/05/08 18:33:27 by nbled            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_list	*state_command_right(t_list *l_start, t_btree *ast_node)
+t_list	*state_command_right(t_list *l_start, t_btree *ast_node
+	, t_list *tmp, t_list *ptr)
 {
-	t_list	*tmp;
-	t_list	*ptr;
-
 	tmp = l_start;
 	ast_node->right = btree_new(NULL, T_COMMAND);
 	ast_node->right->left = btree_new(tmp, T_FUNCTION);
@@ -42,12 +40,11 @@ t_list	*state_command_right(t_list *l_start, t_btree *ast_node)
 	}
 	return (ptr);
 }
+// 2l
 
-t_list	*state_command(t_list *l_start, t_btree *ast_node)
+t_list	*state_command(t_list *l_start, t_btree *ast_node
+	, t_list *tmp, t_list *ptr)
 {
-	t_list	*tmp;
-	t_list	*ptr;
-
 	tmp = l_start;
 	ast_node->left = btree_new(NULL, T_COMMAND);
 	ast_node->left->left = btree_new(tmp, T_FUNCTION);
@@ -93,64 +90,10 @@ t_list	*state_pipe(t_list *l_start, t_btree *ast_node)
 	}
 	if (l_start && l_start->token < T_PIPE)
 	{
-		return (state_command_right(l_start, pipe));
+		return (state_command_right(l_start, pipe, NULL, NULL));
 	}
 	return (l_start);
 }
-
-// a > b c
-
-// root
-// 	rdir
-// 		command
-// 			a
-// 			c
-// 		args
-// 			b
-
-// > b c
-
-// b="test kkchose"
-
-// root
-// 	rdir
-// 		command
-// 			c
-// 		args
-// 			$b
-
-// root
-// 	rdir
-// 		command
-// 			kkchose c
-// 		args
-// 			test
-
-/*
-
-a | b > c
-
-root
-	pipe
-		command
-			fonction a 
-		rdir
-			command 
-				fonction b
-			c
-
- a | b > c > d
-
- root
-	pipe
-		command
-			fonction a
-		rdir
-			rdir
-				b
-				c
-			d
-*/
 
 t_base_result	state_base(t_list *l_start, t_btree *ast_node)
 {
@@ -163,7 +106,7 @@ t_base_result	state_base(t_list *l_start, t_btree *ast_node)
 	while (tmp)
 	{
 		if (tmp->token < T_PIPE)
-			tmp = state_command(tmp, ast_node);
+			tmp = state_command(tmp, ast_node, NULL, NULL);
 		else if (tmp->token >= T_PIPE && tmp->token < T_LPAREN)
 			tmp = state_pipe(tmp, ast_node);
 	}
